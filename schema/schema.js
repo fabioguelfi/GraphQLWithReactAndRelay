621cbb39a82e19c67edbcfbb14c6ef343d0d7e4b
fabio guelfi <fabioguelfiunix@gmail.com>
2018-10-06 17:41:37 -0300

    Root Queries

--- schema/schema.js
+++ schema/schema.js
@@ -15,3 +15,15 @@ const UserType = new GraphQLObjectType({
   }
 })
 
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    }
+  }
+})
