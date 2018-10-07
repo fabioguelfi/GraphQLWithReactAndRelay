7d0037b7d30f5322d7be23e27a761ed2548deefa
fabio guelfi <fabioguelfiunix@gmail.com>
2018-10-06 21:42:08 -0300

    NonNull Fields and Mutations

--- schema/schema.js
+++ schema/schema.js
@@ -82,6 +82,16 @@ const mutation = new GraphQLObjectType({
         return axios.post(`http://localhost:3000/users`, { firstName, age, companyId })
           .then(r => r.data)
       }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve (parentValue, { id }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+          .then(r => r.data)
+      }
     }
   }
 })
